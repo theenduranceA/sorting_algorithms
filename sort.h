5d3a4732f3185260eb6f04535b01e3f97112e8e5
theenduranceA <theenduranceaneke@gmail.com>
2023-07-24 14:57:24 -0700

    Fixed

--- sort.h
+++ sort.h
@@ -18,6 +18,7 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
@@ -32,6 +33,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
 #endif
