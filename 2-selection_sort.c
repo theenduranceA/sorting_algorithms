87064f3d12854ef8d187622f0ab938ded0ea22b1
theenduranceA <theenduranceaneke@gmail.com>
2023-07-25 05:06:30 -0700

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,9 +1,10 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Function that sorts a doubly linked list of
- * integers in ascending order.
- * @list: Double pointer to the head of the list. 
+ * selection_sort - Function that sorts an array of integers in
+ * ascending order using the Selection sort algorithm.
+ * @array: Array of integers to be sorted..
+ * @size: Size of the array.
  *
  */
 
@@ -17,6 +18,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		int *sort1 = array + i;
+
 		for (j = i + 1; j < size; j++)
 			sort1 = (array[j] < *sort1) ? (array + j) : sort1;
 
